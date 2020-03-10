980e160e13acce3c5903fc060da330b20889c834
yoon <yyoooona@gmail.com>
2020-03-11 01:00:16 +0900

    Updates

---
+++ precache-manifest.f2cd2ef5a0746c97cfe8ee8d7262ef5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "787551e2988523cabb5baab7295b5c9a",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "126b3bfa8342c2f145ff",
+    "url": "/movie_app_2020/static/css/main.45e62dbb.chunk.css"
+  },
+  {
+    "revision": "5d99528de9efa172eab7",
+    "url": "/movie_app_2020/static/js/2.3f490354.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/movie_app_2020/static/js/2.3f490354.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "126b3bfa8342c2f145ff",
+    "url": "/movie_app_2020/static/js/main.7783375f.chunk.js"
+  },
+  {
+    "revision": "5b9de689d76c950937df",
+    "url": "/movie_app_2020/static/js/runtime-main.db2dcdc9.js"
+  }
+]);
